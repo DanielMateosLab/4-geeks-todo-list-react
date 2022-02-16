4935cfcf99ed66db464b1c1bfce90427e1ed251c
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-02-16 19:18:21 +0000

    Add documentation to Papers

--- src/js/component/Papers.jsx
+++ src/js/component/Papers.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+/** Wraps the children elements in a paper that sits over two more papers */
 const Papers = ({ children }) => (
 	<div className="paper parent-paper">
 		<div className="paper overhead">
