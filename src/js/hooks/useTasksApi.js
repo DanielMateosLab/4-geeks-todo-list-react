17982c2c61d03007747527e80ad6f3f2b2daadfd
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-02-18 19:22:25 +0000

    Solve bug deleteing last task. Create removeAllTasks()

--- src/js/hooks/useTasksApi.js
+++ src/js/hooks/useTasksApi.js
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
 
+// There is a bug in the API that does not let us set an empty task list --> []
+// Also, when we create a task list we get a sample task, so there is no way to have
+// an empty task list.
+// To work-around this, we will always ignore the last (oldest) task when getting them
+// Also, we put an empty task when updating the list
+
 const useTasksApi = () => {
 	const uri =
 		"https://assets.breatheco.de/apis/fake/todos/user/danielMateosLab";
@@ -12,16 +18,20 @@ const useTasksApi = () => {
 	async function getTasks() {
 		const newTasks = await (await fetch(uri)).json();
 
-		setTasks(newTasks);
+		setTasks(newTasks.slice(0, -1));
 	}
 
 	async function updateTasks(newTasks) {
+		// We need to add this empty task to the end of the array
+		// to work-around the buggy API as described above
+		const emptyTask = { label: "A", done: false };
+
 		await fetch(uri, {
 			method: "PUT",
 			headers: {
 				"Content-Type": "application/json",
 			},
-			body: JSON.stringify(newTasks),
+			body: JSON.stringify([...newTasks, emptyTask]),
 		});
 
 		await getTasks();
@@ -40,10 +50,15 @@ const useTasksApi = () => {
 		await updateTasks(newTasks);
 	}
 
+	async function removeAllTasks() {
+		await updateTasks([]);
+	}
+
 	return {
 		tasks,
 		addTask,
 		removeTask,
+		removeAllTasks,
 	};
 };
 
