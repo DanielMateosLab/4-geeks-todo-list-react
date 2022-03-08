ce9aac08f620144559df4ecdad41d4ab2349635d
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-08 22:42:42 +0100

    Fix buggy build

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,60 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 var PrettierPlugin = require("prettier-webpack-plugin");
-const ESLintPlugin = require('eslint-webpack-plugin');
-const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
 
 const port = 3000;
 let publicUrl = `ws://localhost:${port}/ws`;
-if(process.env.GITPOD_WORKSPACE_URL){
-  const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split('://');
+if (process.env.GITPOD_WORKSPACE_URL) {
+  const [schema, host] = process.env.GITPOD_WORKSPACE_URL.split("://");
   publicUrl = `wss://${port}-${host}/ws`;
 }
-console.log("publicUrl", publicUrl)
+console.log("publicUrl", publicUrl);
 
 module.exports = {
-  entry: [
-    './src/js/index.js'
-  ],
+  entry: path.join(__dirname, "src", "js", "index.js"),
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/'
+    path: path.resolve(__dirname, "public"),
   },
   module: {
     rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
         },
-        {
-          test: /\.(css)$/, use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-          }]
-        }, //css only files
-        { 
-          test: /\.(png|svg|jpg|gif)$/, use: {
-            loader: 'file-loader',
-            options: { name: '[name].[ext]' } 
-          }
-        }, //for images
-        { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] } //for fonts
-    ]
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          {
+            loader: "style-loader", // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+          },
+        ],
+      }, //css only files
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: { name: "[name].[ext]" },
+        },
+      }, //for images
+      {
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+        use: ["file-loader"],
+      }, //for fonts
+    ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
-    modules: [ "node_modules", path.resolve(__dirname, 'src') ],
+    extensions: ["*", ".js", ".jsx"],
+    modules: ["node_modules", path.resolve(__dirname, "src")],
   },
   devtool: "source-map",
   devServer: {
@@ -59,7 +66,7 @@ module.exports = {
       directory: path.resolve(__dirname, "dist"),
     },
     client: {
-      webSocketURL: publicUrl
+      webSocketURL: publicUrl,
     },
   },
   plugins: [
@@ -68,19 +75,19 @@ module.exports = {
     //   files: path.resolve(__dirname, "src"),
     // }),
     new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
-        template: 'template.html'
+      favicon: "4geeks.ico",
+      template: path.join(__dirname, "template.html"),
     }),
     new PrettierPlugin({
       parser: "babel",
-      printWidth: 80,             // Specify the length of line that the printer will wrap on.
-      tabWidth: 4,                // Specify the number of spaces per indentation-level.
-      useTabs: true,              // Indent lines with tabs instead of spaces.
+      printWidth: 80, // Specify the length of line that the printer will wrap on.
+      tabWidth: 4, // Specify the number of spaces per indentation-level.
+      useTabs: true, // Indent lines with tabs instead of spaces.
       bracketSpacing: true,
-      extensions: [ ".js", ".jsx" ],
+      extensions: [".js", ".jsx"],
       jsxBracketSameLine: true,
-      semi: true,                 // Print semicolons at the ends of statements.
-      encoding: 'utf-8'           // Which encoding scheme to use on files
+      semi: true, // Print semicolons at the ends of statements.
+      encoding: "utf-8", // Which encoding scheme to use on files
     }),
-  ]
+  ],
 };
